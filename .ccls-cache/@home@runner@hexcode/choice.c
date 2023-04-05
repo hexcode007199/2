b0de5e6e6de67a0e306e76ae9f2da9abb08b11e6
31125 <>
2023-04-05 00:19:03 +0000

    choice

---
+++ .ccls-cache/@home@runner@hexcode/choice.c
@@ -0,0 +1,19 @@
+// #include <stdio.h>
+
+// main(){
+//   int a=5, b=3;
+
+//   if (5>3){//if true
+//     printf("A\n");
+//       printf("b\n");}
+
+//   printf("C\n");//true
+
+//   if(5<3){
+//     printf("D\n");
+//     printf("E\n");
+  
+//   }
+//   printf("F\n");
+  
+// }
---
+++ .ccls-cache/@home@runner@hexcode/relu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int relu(a);
+  return np.maximum(0,x)
+
+    x=np.arange(-5.0, 5.0, 0.1)
+    y=relu(x)
+
+    plt.plot(x, y)
+    plt.plot([0,0],[5.0,0.0],':')
+    plt.title('ReLU Function')
+    plt.show()
